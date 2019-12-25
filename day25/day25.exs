489caf0fa11efc2018ec29f33211b2a539880e4b
George Roman <george.roman.99@gmail.com>
2019-12-25 23:28:06 +0200

    Add day 25 solution

---
+++ day25/day25.exs
@@ -0,0 +1,154 @@
+defmodule Day25 do
+  defp select_mode_in(opcodes, modes, base, i, count) do
+    if length(modes) > count do
+      case Enum.at(modes, count) do
+        "1" -> opcodes[i] || 0
+        "2" -> opcodes[base + (opcodes[i] || 0)] || 0
+        _ -> opcodes[opcodes[i]] || 0
+      end
+    else
+      opcodes[opcodes[i]] || 0
+    end
+  end
+
+  defp select_mode_out(opcodes, modes, base, i, count) do
+    if length(modes) > count and Enum.at(modes, count) == "2" do
+      base + (opcodes[i] || 0)
+    else
+      opcodes[i] || 0
+    end
+  end
+
+  def run_opcodes(opcodes, i, base, input \\ nil, output \\ "") do
+    s_opcode = Integer.to_string(opcodes[i])
+
+    opcode =
+      if(String.length(s_opcode) >= 2, do: String.slice(s_opcode, -2..-1), else: s_opcode)
+      |> String.to_integer()
+
+    modes = Regex.scan(~r/./, s_opcode |> String.slice(0..-3)) |> List.flatten() |> Enum.reverse()
+
+    case opcode do
+      1 ->
+        op1 = select_mode_in(opcodes, modes, base, i + 1, 0)
+        op2 = select_mode_in(opcodes, modes, base, i + 2, 1)
+        out = select_mode_out(opcodes, modes, base, i + 3, 2)
+        run_opcodes(Map.put(opcodes, out, op1 + op2), i + 4, base, input, output)
+
+      2 ->
+        op1 = select_mode_in(opcodes, modes, base, i + 1, 0)
+        op2 = select_mode_in(opcodes, modes, base, i + 2, 1)
+        out = select_mode_out(opcodes, modes, base, i + 3, 2)
+        run_opcodes(Map.put(opcodes, out, op1 * op2), i + 4, base, input, output)
+
+      3 ->
+        if input == nil or input === [] do
+          {:input, opcodes, i, base, output}
+        else
+          out = select_mode_out(opcodes, modes, base, i + 1, 0)
+          run_opcodes(Map.put(opcodes, out, hd(input)), i + 2, base, tl(input), output)
+        end
+
+      4 ->
+        current_output = select_mode_in(opcodes, modes, base, i + 1, 0)
+        run_opcodes(opcodes, i + 2, base, input, output <> List.to_string([current_output]))
+
+      5 ->
+        op1 = select_mode_in(opcodes, modes, base, i + 1, 0)
+        op2 = select_mode_in(opcodes, modes, base, i + 2, 1)
+
+        run_opcodes(
+          opcodes,
+          if op1 != 0 do
+            op2
+          else
+            i + 3
+          end,
+          base,
+          input,
+          output
+        )
+
+      6 ->
+        op1 = select_mode_in(opcodes, modes, base, i + 1, 0)
+        op2 = select_mode_in(opcodes, modes, base, i + 2, 1)
+
+        run_opcodes(
+          opcodes,
+          if op1 == 0 do
+            op2
+          else
+            i + 3
+          end,
+          base,
+          input,
+          output
+        )
+
+      7 ->
+        op1 = select_mode_in(opcodes, modes, base, i + 1, 0)
+        op2 = select_mode_in(opcodes, modes, base, i + 2, 1)
+        out = select_mode_out(opcodes, modes, base, i + 3, 2)
+
+        run_opcodes(
+          Map.put(
+            opcodes,
+            out,
+            if op1 < op2 do
+              1
+            else
+              0
+            end
+          ),
+          i + 4,
+          base,
+          input,
+          output
+        )
+
+      8 ->
+        op1 = select_mode_in(opcodes, modes, base, i + 1, 0)
+        op2 = select_mode_in(opcodes, modes, base, i + 2, 1)
+        out = select_mode_out(opcodes, modes, base, i + 3, 2)
+
+        run_opcodes(
+          Map.put(
+            opcodes,
+            out,
+            if op1 == op2 do
+              1
+            else
+              0
+            end
+          ),
+          i + 4,
+          base,
+          input,
+          output
+        )
+
+      9 ->
+        param = select_mode_in(opcodes, modes, base, i + 1, 0)
+        run_opcodes(opcodes, i + 2, base + param, input, output)
+
+      99 ->
+        {:halt, nil, nil, nil, output}
+    end
+  end
+end
+
+opcodes =
+  File.read!("day25.input")
+  |> String.split(",")
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&String.to_integer/1)
+  |> Enum.with_index(0)
+  |> Enum.reduce(%{}, fn {op, i}, acc -> Map.put(acc, i, op) end)
+
+Stream.iterate(Day25.run_opcodes(opcodes, 0, 0), fn {_, opcodes, last_index, base, output} ->
+  IO.puts(output)
+  command = IO.gets("")
+
+  Day25.run_opcodes(opcodes, last_index, base, String.to_charlist(command))
+end)
+|> Enum.find(fn {status, _, _, _, output} -> status == :halt end)
